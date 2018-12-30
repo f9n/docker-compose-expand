d46ac81fdf6a269a16886237d4a43a184683d71d
f9n <f9n@protonmail.com>
2018-12-30 16:47:00 +0300

    Display version of docker-compose-expand

--- docker_compose_expand/cli.py
+++ docker_compose_expand/cli.py
@@ -5,6 +5,9 @@ from . import __version__
 
 
 def main():
+    if len(sys.argv) > 1 and sys.argv[1] in ["version"]:
+        print("docker-compose-expand version {}".format(__version__))
+
     subprocess.call(["docker-compose", *sys.argv[1:]])
     sys.exit(0)
 
