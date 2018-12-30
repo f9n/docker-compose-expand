c35e89b2413e7906b6b3fc8e6c39b5cffaa95e43
f9n <f9n@protonmail.com>
2018-12-30 18:51:20 +0300

    Generate new services with expandable services for the docker-compose.yml file

--- docker_compose_expand/core.py
+++ docker_compose_expand/core.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 from . import __version__
-from .utils import yaml
+from .utils import yaml, template
 
 
 class DockerComposeExpand:
@@ -39,7 +39,38 @@ class DockerComposeExpand:
 
     def generate(self):
         """ Generate services based on the expand field in the docker-compose-expand.yml file"""
-        pass
+        for defined_expandable_service in self.defined_expandable_services:
+            expanded_services = self.__generate_expandable_services(
+                name_field=defined_expandable_service["name"],
+                service_field=defined_expandable_service["service"],
+                loop_field=defined_expandable_service["loop"],
+            )
+            for service in expanded_services:
+                self.real_services.update(service)
+
+    def __generate_service(self, name, service, item):
+        _name = template.render(name, item)
+        _new_service = template.render(service, item)
+        return {_name: _new_service}
+
+    def __generate_services(self, variables, name_field, service_field):
+        _services = []
+        for item in variables:
+            _new_service_object = self.__generate_service(
+                name_field, service_field, item
+            )
+            _services.append(_new_service_object)
+
+        return _services
+
+    def __generate_expandable_services(self, name_field, service_field, loop_field):
+        expanded_loop_field = loop_field
+        if isinstance(loop_field, str):
+            expanded_loop_field = template.render(loop_field, self.variables)
+        elif isinstance(loop_field, list):
+            pass
+
+        return self.__generate_services(expanded_loop_field, name_field, service_field)
 
     def save(self):
         """ Save the services to docker-compose.yml file """
